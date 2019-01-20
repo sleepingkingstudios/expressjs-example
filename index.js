0586414ed790f9ce4b6479b92677069bce07a82d
Rob Smith <merlin@sleepingkingstudios.com>
2019-01-20 02:54:23 -0500

    Add eslint dev dependency.

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.get('/api', (request, response) => {
-  response.json({ 'ok': true });
+  response.json({ ok: true });
 });
 
 app.listen(port);
 
-console.log(`Listening on port ${port}`);
+console.log(`Listening on port ${port}`); // eslint-disable-line no-console
