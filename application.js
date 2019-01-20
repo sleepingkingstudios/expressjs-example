d14c67ea603bae43f618f6ddbaf6ec03959259f1
Rob Smith <merlin@sleepingkingstudios.com>
2019-01-20 17:03:38 -0500

    Add React application in /client.

--- application.js
+++ application.js
@@ -1,9 +1,19 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 
 app.get('/api', (request, response) => {
   response.json({ ok: true });
+});
+
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+// The "catchall" handler: for any request that doesn't
+// match one above, send back React's index.html file.
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/client/build/index.html'));
 });
 
 module.exports = app;
