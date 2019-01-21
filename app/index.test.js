a180978560511b15bf78869d047ca610372cb4d1
Rob Smith <merlin@sleepingkingstudios.com>
2019-01-21 03:04:55 -0500

    Ensure supertest terminates requests.

--- app/index.test.js
+++ app/index.test.js
@@ -3,15 +3,19 @@ const request = require('supertest');
 const app = require('./index');
 
 describe('GET /api', () => {
-  it('should respond with 200 OK', async () => {
+  it('should respond with 200 OK', async (done) => {
     const response = await request(app).get('/api');
 
     expect(response.statusCode).toBe(200);
+
+    done();
   });
 
-  it('should respond with { ok: true }', async () => {
+  it('should respond with { ok: true }', async (done) => {
     const response = await request(app).get('/api');
 
     expect(response.body).toEqual({ ok: true });
+
+    done();
   });
 });
